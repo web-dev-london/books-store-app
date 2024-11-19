4879444a168177eca33aa2bd71ae7a181cad45c7
Murod Akhmedov <murod.london@gmail.com>
2024-11-19 10:28:22 +0000

    Hide google api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,46 +1,46 @@
 import axios, { AxiosRequestConfig } from "axios";
 import z from "zod";
 import {
-    defineFetchResponse,
-    FetchResponse,
-    validateResponse,
+  defineFetchResponse,
+  FetchResponse,
+  validateResponse,
 } from "../validation/validate";
 
 const axiosInstance = axios.create({
-    baseURL: 'https://www.googleapis.com',
-    params: {
-        key: `AIzaSyDSo8ewzkQCnp4MiHC7fsJfGyGDBLcASEU`,
-    }
+  baseURL: 'https://www.googleapis.com',
+  params: {
+    key: import.meta.env.VITE_API_KEY,
+  }
 });
 
 
 class ApiClient<T> {
-    endpoint: string;
-    schema: z.ZodSchema<T>;
+  endpoint: string;
+  schema: z.ZodSchema<T>;
 
-    constructor(endpoint: string, schema: z.ZodSchema<T>) {
-        this.endpoint = endpoint
-        this.schema = schema
-    }
+  constructor(endpoint: string, schema: z.ZodSchema<T>) {
+    this.endpoint = endpoint
+    this.schema = schema
+  }
 
-    getAll = async (config?: AxiosRequestConfig): Promise<FetchResponse<T>> => {
-        const response = await axiosInstance.get<unknown>(this.endpoint, config);
-        // console.log('Books API Response: ', response.data);
-        const fetchResponseSchema = defineFetchResponse(this.schema);
-        // console.log('Fetch Response Schema: ', fetchResponseSchema);
-        return validateResponse(fetchResponseSchema, response.data);
-    }
+  getAll = async (config?: AxiosRequestConfig): Promise<FetchResponse<T>> => {
+    const response = await axiosInstance.get<unknown>(this.endpoint, config);
+    // console.log('Books API Response: ', response.data);
+    const fetchResponseSchema = defineFetchResponse(this.schema);
+    // console.log('Fetch Response Schema: ', fetchResponseSchema);
+    return validateResponse(fetchResponseSchema, response.data);
+  }
 
-    get = async (id: string): Promise<T> => {
-        const response = await axiosInstance.get<unknown>(`${this.endpoint}/${id}`);
-        // console.log('Book Detail API Response: ', response.data);
-        const validation = this.schema.safeParse(response.data);
-        if (!validation.success) {
-            console.error("Validation error in GET:", validation.error);
-            throw new Error("Invalid data structure received from the API.");
-        }
-        return validation.data;
+  get = async (id: string): Promise<T> => {
+    const response = await axiosInstance.get<unknown>(`${this.endpoint}/${id}`);
+    // console.log('Book Detail API Response: ', response.data);
+    const validation = this.schema.safeParse(response.data);
+    if (!validation.success) {
+      console.error("Validation error in GET:", validation.error);
+      throw new Error("Invalid data structure received from the API.");
     }
+    return validation.data;
+  }
 }
 
 export default ApiClient;
